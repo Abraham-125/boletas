81e2d3d736808b89b36e7df2e4c8fb6d10753972
abraham-125 <abrahamgutierrez1999@gmail.com>
2024-07-08 02:56:53 -0400

    añadi detalles de boletas modal

---
+++ tu-archivo-json.js
@@ -0,0 +1,9 @@
+// tu-archivo-json.js
+const boletaData = [
+    {
+        "producto": "Shampoo",
+        "cantidad": "1kg",
+        "precio": 2000
+    },
+    // Agrega más productos aquí...
+];
